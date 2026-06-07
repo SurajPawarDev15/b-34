e7d9e49c7599eb160cd50b2df78bd57d1e4953c8
SurajPawarDev15 <supersuraj143@gmail.com>
2026-06-07 23:31:06 +0530

    done with practices

---
+++ src/app/components/my-data/my-data.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-data',
+  imports: [],
+  templateUrl: './my-data.html',
+  styleUrl: './my-data.css',
+})
+export class MyData {}
