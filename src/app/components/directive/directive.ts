a94b98c7c0055ba1e3163ef8162665d3ed1dbe21
SurajPawarDev15 <supersuraj143@gmail.com>
2026-06-10 23:54:45 +0530

    first commit

---
+++ src/app/components/directive/directive.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directive',
+  imports: [],
+  templateUrl: './directive.html',
+  styleUrl: './directive.css',
+})
+export class Directive {}
