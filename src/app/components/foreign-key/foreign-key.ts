89a314f4d01d594032ceda2d5c53cc9567b5afbb
SurajPawarDev15 <supersuraj143@gmail.com>
2026-06-29 22:28:56 +0530

    added

---
+++ src/app/components/foreign-key/foreign-key.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-foreign-key',
+  imports: [],
+  templateUrl: './foreign-key.html',
+  styleUrl: './foreign-key.css',
+})
+export class ForeignKey {}
