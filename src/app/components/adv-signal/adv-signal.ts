0e1bfb2e88a2caf38bd2af39811fb170cc75ad60
SurajPawarDev15 <supersuraj143@gmail.com>
2026-07-01 08:38:58 +0530

    added

---
+++ src/app/components/adv-signal/adv-signal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adv-signal',
+  imports: [],
+  templateUrl: './adv-signal.html',
+  styleUrl: './adv-signal.css',
+})
+export class AdvSignal {}
---
+++ src/app/components/pipe-ex/pipe-ex.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe-ex',
+  imports: [],
+  templateUrl: './pipe-ex.html',
+  styleUrl: './pipe-ex.css',
+})
+export class PipeEx {}
