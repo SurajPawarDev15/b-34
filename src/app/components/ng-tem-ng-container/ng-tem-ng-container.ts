cc0097482b7b955b51a419e113944d5773b87e02
SurajPawarDev15 <supersuraj143@gmail.com>
2026-07-02 08:20:58 +0530

    added

---
+++ src/app/components/ng-tem-ng-container/ng-tem-ng-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ng-tem-ng-container',
+  imports: [],
+  templateUrl: './ng-tem-ng-container.html',
+  styleUrl: './ng-tem-ng-container.css',
+})
+export class NgTemNgContainer {}
---
+++ src/app/components/viewchild-ex/viewchild-ex.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-viewchild-ex',
+  imports: [],
+  templateUrl: './viewchild-ex.html',
+  styleUrl: './viewchild-ex.css',
+})
+export class ViewchildEx {}
