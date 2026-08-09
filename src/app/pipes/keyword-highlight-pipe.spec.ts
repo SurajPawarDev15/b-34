7ad2e8ac085f57918544c9d20ff76381bb3db42b
Suraj Sanjeev Pawar <67969261+SurajPawarDev15@users.noreply.github.com>
2026-08-09 23:44:03 +0530

    added

---
+++ src/app/pipes/keyword-highlight-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { KeywordHighlightPipe } from './keyword-highlight-pipe';
+
+describe('KeywordHighlightPipe', () => {
+  it('create an instance', () => {
+    const pipe = new KeywordHighlightPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
