afc507634c8a1fea5cdfaf5bada30c575ce4f0d7
Suraj Sanjeev Pawar <67969261+SurajPawarDev15@users.noreply.github.com>
2026-08-16 23:43:22 +0530

    added

---
+++ src/app/pipes/sum-pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sum',
+})
+export class SumPipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+}
