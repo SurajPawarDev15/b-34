58a7f9e23bcb6bf26a7c8ef8bbb6fb74701d7b8b
Suraj Sanjeev Pawar <67969261+SurajPawarDev15@users.noreply.github.com>
2026-08-18 23:47:59 +0530

    added

---
+++ src/app/pipes/heena-pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HeenaPipe } from './heena-pipe';
+
+describe('HeenaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HeenaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
